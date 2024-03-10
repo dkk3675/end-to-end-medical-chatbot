6dab56135dccefa236f1b049f22784fa9ff53c6c
Divya <karan.boss1000@gmail.com>
2024-03-10 18:12:51 +0530

    all updates

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Divya',
+    author_email= 'kumar.divya3675@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
